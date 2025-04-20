feed54023f9868d2c569249380293d30e2f15076
killbaz <amrifikri02@gmail.com>
2025-04-20 15:41:32 +0700

    up

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('notes_db', 'root', '', {
+const db = new Sequelize('RECOVER_YOUR_DATA', 'root', '', {
     host: '34.121.249.104',
     dialect: 'mysql'
 });
