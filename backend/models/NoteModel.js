84751635a7ddc80732f3a75267fe04062307afcf
killbaz <amrifikri02@gmail.com>
2025-05-26 18:51:08 +0700

    up be

--- backend/models/NoteModel.js
+++ backend/models/NoteModel.js
@@ -1,20 +1,17 @@
 import { DataTypes } from "sequelize";
 import db from "../config/database.js";
 
-const Note = db.define(
-  "notes",{ 
-    title: {type: Sequelize.STRING,},
-    description: {type: Sequelize.STRING,},
-  },
-  {
-    freezeTableName: true,
-    createdAt: "tanggal_dibuat",
-    updatedAt: "tanggal_diperbarui",
-  }
-);
+const Note = db.define("notes", {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    }
+}, {
+    timestamps: false
+});
 
 export default Note;
-
-(async () => {
-  await db.sync();
-})();
